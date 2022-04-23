1bdd537eb90885bdeeb10e4471744d49ed933e1a
AnJuHwan <wnghks0531@gmail.com>
2022-04-23 09:05:23 +0900

    cors

--- dist/app.js
+++ dist/app.js
@@ -20,7 +20,7 @@ mongoose_1.default.connect(`${process.env.MONGODB_URI}`)
 })
     .catch((error) => console.log(error));
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({ origin: ['*'] }));
+app.use((0, cors_1.default)({ origin: ['*'], credentials: true }));
 app.use((0, cookie_parser_1.default)());
 app.use('/post', postRouter_1.default);
 app.use('/users', userRouter_1.default);
